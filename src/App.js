082f8d7f4ae3f9f2a34eb8de04bc21916ccfe62c
Nathakorn Sukumsirichart <nathakorn.sksrc@gmail.com>
2020-12-20 23:28:04 +0700

    add input-card-modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from "react";
-// import { Button, DatePicker } from "antd";
+import { Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import Card from "./components/Card";
 import "./antd.less";
 
 class App extends React.Component {
+  state = {
+    inputCardModal: false,
+  };
+  showInputCardModal = () => {
+    this.setState({ inputCardModal: !this.state.inputCardModal });
+    console.log("showInputCard");
+  };
   render() {
+    const { inputCardModal } = this.state;
     return (
       <div className="container">
         <header className="header">
-          <div className="logo-section">Youknow</div>
-          <div className="menu">menu</div>
+          <div className="logo-section">You know</div>
+          <div className="menu">
+            <Button
+              type="primary"
+              onClick={this.showInputCardModal}
+              icon={<PlusOutlined />}
+              size="large"
+            >
+              New Card
+            </Button>
+          </div>
         </header>
         <div className="content">
           <nav className="sidebar">sidebar</nav>
@@ -20,6 +38,12 @@ class App extends React.Component {
             <Card number={4} />
           </main>
         </div>
+        {inputCardModal && (
+          <div className="input-card-modal">
+            <div className="input-card-container">test</div>
+          </div>
+        )}
+        {/* <div className="input-card-modal">input modal</div> */}
       </div>
     );
   }
