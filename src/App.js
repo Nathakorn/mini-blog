22adf59a0f31ff528ff3280d8b1d1be4ab3338e4
Nathakorn Sukumsirichart <nathakorn.sksrc@gmail.com>
2020-12-20 20:50:04 +0700

    add global variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, DatePicker } from "antd";
 
-import "./App.scss";
+import "./sass/App.scss";
 import "./App.less";
 
 class App extends React.Component {
